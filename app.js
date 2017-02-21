e312b8d4d1d66bea610c6032884f57e0c851b05b
Thomas Johander <lopan@Thomas-Johanders-MacBook-Pro.local>
2017-02-21 17:04:03 -0600

    Lots more example work

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
 //node ALWAYS runs on the server
 
 var express = require('express');
-var app = epxress();
+var app = express();
+var bodyParser = require('body-parser');
 
 //app.use runs everytime a request comes in
 //if it's looking for a static file, (index.html, client.js, style.css, etc..),
 //look in folder 'public'
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+var songList = [
+  {
+    title: 'We did not start the Phire',
+    artist: 'William Joel'
+  },
+  {
+    title: 'Ring of Fire',
+    artist: 'Johnny Cash'
+  }
+];
+
+
+//app.get runs WHENEVER the server gets an HTTP GET request
+app.get('/songs',function(req,res){
+  res.send(songList)
+})
+
+app.post('/newSong', function(req,res){
+  var newSong = req.body; //'data' from the client side
+  if(newSong.artist !== 'Justin Bieber'){
+  songList.push(newSong);
+  console.log(songList);
+  res.sendStatus(200); //everytime you send a request, you need to send a response
+} else {
+  res.sendStatus(500);
+}
+});
+
+
 
 //app.listen runs ONCE at startup
 app.listen(3000);
--- public/client.js
+++ public/client.js
@@ -2,6 +2,50 @@
 
 $(document).ready(function(){
   console.log('jQuery was successfully sourced');
-})
+  getAllSongs();
+  function getAllSongs(){
+    $.ajax({
+      type: 'GET',
+      url: '/songs',
+      success: function(response) {
+        console.log('response', response);
+      },
+      error: function(error) {
+        console.log('there was an error');
+      }
 
-//end of file
+    });
+  }
+
+
+
+  $('#addSongButton').on('click',function(){
+    var newSongTitle = $('#title').val();
+    var newSongArtist = $('#artist').val();
+    var newSongObject = {
+      title: newSongTitle,
+      artist: newSongArtist
+    };
+    console.log(newSongObject);
+    $.ajax({
+      type: "POST",
+      url: '/newSong',
+      data: newSongObject,
+      success: function(response){
+        console.log('response is ',response);
+        getAllSongs()
+      },
+      error: function(error) {
+        console.log('there was an error');
+      }
+
+
+    })
+  })
+
+
+
+
+
+
+});//end of document ready
